bb3fea38dd34d63bbbb313d75f4fae6aadf8cb11
Yugen Limbu <2407570@live.stmarys.ac.uk>
2024-11-27 17:02:02 +0000

    Added the Show transaction function

--- assessment2/modules/ui.py
+++ assessment2/modules/ui.py
@@ -1,8 +1,16 @@
-def printcolumn(list, var1):
-    print("|", "=" * 28, "|")
-    print(f"| {var1}", " "*(27-len(var1)), "|")
-    print("|", "="*28, "|")
-    rowLen = 27
+# Function to print a column
+def printcolumn(list, var1, length):
+    print("|", "=" * length, "|")
+    print(f"| {var1}", " "*(length-len(var1)-1), "|")
+    print("|", "="*length, "|")
     for i in list:
-        print("| ", i, " "*(rowLen-len(i)-1),"|")
-    print("|", "="*28, "|\n")
+        print("| ", i, " "*(length-len(i)-2),"|")
+    print("|", "="*length, "|\n")
+
+def printTransactions(list, header, length):
+    
+    print("|", "=" * length, "|")
+    for i in range(len(header)):
+        x = f"{header[i]}: {list[i]}"
+        print("| ", x, " "*(length-2-len(x)), "|")
+    print("|", "="*length, "|\n")
