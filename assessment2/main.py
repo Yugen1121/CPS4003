59d6a8821cfdb48af613445f30c42b09c76b7e72
Yugen Limbu <2407570@live.stmarys.ac.uk>
2024-11-27 15:57:56 +0000

    Added some function for testing

--- assessment2/main.py
+++ assessment2/main.py
@@ -1,5 +1,11 @@
+from modules import datas
+from modules import ui
+
 
 def main():
+    transactions, store_location, category, header = datas.datas()
+    ui.printcolumn(store_location, "Store Location")
+    ui.printcolumn(category, "Categories")
     pass
 
 if __name__ == "__main__":
