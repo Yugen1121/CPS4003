3ab38ed1ae5ae995e6a069a781465be1a65a66a0
Yugen Limbu <2407570@live.stmarys.ac.uk>
2024-11-27 15:34:04 +0000

    fixed a bug

--- assessment2/modules/datas.py
+++ assessment2/modules/datas.py
@@ -6,7 +6,7 @@ path = os.getcwd()
 def datas():
     with open(f"{path}/files/retail_sales_data.csv") as file:
         rows = csv.reader(file)
-        headers = rows
+        header = next(rows)
         transactions = {}
         store_location = {}
         category = {}
@@ -16,11 +16,11 @@ def datas():
                 store_location[row[2]].append(row[0])
             except KeyError:
                 store_location[row[2]] = []
-                store_location.append[row[2]]
+                store_location[row[2]].append(row[0])
              
             try:
                 category[row[3]].append(row[0])
             except KeyError:
                 category[row[3]] = []
-                category.append[row[2]]
-        return (transactions, store_location, category)
+                category[row[3]].append(row[0])
+        return transactions, store_location, category
