50869b6991e15b0cf92dee5927ee07a6a9d8dfe7
Yugen1121 <163615963+Yugen1121@users.noreply.github.com>
2024-11-29 12:20:15 +0000

    Added the function to print a simple table

--- assessment2/modules/ui.py
+++ assessment2/modules/ui.py
@@ -14,3 +14,31 @@ def printTransactions(list, header, length):
         x = f"{header[i]}: {list[i]}"
         print("| ", x, " "*(length-2-len(x)), "|")
     print("|", "="*length, "|\n")
+
+def print_row(length, values, sign):
+    for value in values:
+        for i in range(len(value)):
+            print("| ", value[i], " "*(length-3-len(str(value[i]))), end="")
+        print("|")
+
+    for i in range(len(values[0])):
+        print("|", f"{sign}"*(length-1), end="")
+    print("|")
+
+def print_header(list, length):
+    for i in list:
+        print("|", "="*(length-1), end="")
+    
+
+    for i in list:
+        print("| ", i, " "*(length-len(i)-2), end="")
+    print("|")
+
+
+def print_table_revenue(transaction, store, length):
+    x = []
+    print("|", "="*(length-2), "|", "="*(length-2), "|")
+    print_row(length, [["Store Location", "Revenue"]], "=")
+    for i in store.keys():
+        x.append([i, sum([float(y) for y in [transaction[z][11] for z in [id for id in store[i]]]])])
+    print_row(length, x, "-")
