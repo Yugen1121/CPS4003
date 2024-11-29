ce9265003615c522807683837b06849b7661d12c
Yugen1121 <163615963+Yugen1121@users.noreply.github.com>
2024-11-29 18:48:43 +0000

    added some options to visualise data

--- assessment2/main.py
+++ assessment2/main.py
@@ -1,6 +1,7 @@
 from modules import datas
 from modules import helpers as hel
 from modules import ui
+import matplotlib.pyplot as mp
 
 menues = {1: """1. Retrieve the total number of transactions.
 2. Retrieve unique store locations and product categories.
@@ -9,17 +10,21 @@ menues = {1: """1. Retrieve the total number of transactions.
 5. Retrieve all transactions for a specific product category.
 6. Group transactions by store location and calculate the total revenue per location.
 7. Provide a summary of sales for a specific store location
+8. Visualize The data
 0. Exit"""}
 
 def main():
     transactions, store_location, category, header = datas.datas()
     option = input("Enter the option\n"+menues[1]+"\n")
-    option = hel.invalid_option(option, 7)
+    option = hel.invalid_option(option, 8)
+    
     if option == 1:
         print(f"The Total number of transaction is {len(transactions)}")
+    
     elif option == 2:
         ui.printcolumn(store_location.keys(), "Store locations", 40)
         ui.printcolumn(category.keys(), "Categories", 40)
+    
     elif option == 3:
         while True:
             id = input("Enter the transactionId you are looking for: ")
@@ -33,7 +38,6 @@ def main():
                     break
 
     elif option == 4:
-
         while True:
             name = input("Enter the Store name: ")
             try:
@@ -45,6 +49,7 @@ def main():
                 option = input("Would you like to exit?(y/n) ").lower()
                 if option == "y":
                     break
+    
     elif option == 5:
         while True:
             name = input("Enter the product Category: ")
@@ -57,10 +62,11 @@ def main():
                 option = input("Would you like to exit?(y/n) ").lower()
                 if option == "y":
                     break
+    
     elif option == 6:
         ui.print_table_revenue(transactions, store_location, 40)
-    elif option == 7:
 
+    elif option == 7:
         x = datas.sales_report(transactions)
         payment_method = x[5]
         print(f"""
@@ -72,8 +78,25 @@ SALES REPORT
 • Average customer satisfaction score: {x[4]:.2f}""")
         for i in payment_method.keys():
             print(f"\t-{i}: {payment_method[i]}%")
+    elif option == 8:
+        option = input("""1. Display a pie chart of revenue contribution by store location.
+2. Display a histogram of total transaction values.
+3. An interactive dashboard summarising key insights and trends and
+allowing further exploration of the data.
+0. Exit
+""")
+        option = hel.invalid_option(option, 3)
+        if option == 1:
+            ui.print_pie(datas.total_revenue_per_location(transactions, store_location), "Pie chart of revenue contribution by store location.")
+        elif option == 2:
+            ui.print_hist(transactions, "Histogram of total transactions", "Total transaction", "Frequency of transaction amount")
+        elif option == 3:
+            pass
+        elif option == 0:
+            pass
     elif option == 0:
         print("Thank you!!")
         return True
+    main()
 if __name__ == "__main__":
     main()
