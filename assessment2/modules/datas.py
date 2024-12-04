93b88e90ada232dc2fd898ffdf118765086c40b4
Yugen1121 <163615963+Yugen1121@users.noreply.github.com>
2024-12-04 17:12:29 +0000

    added transaction, total_revenue function to transaction class

--- assessment2/modules/datas.py
+++ assessment2/modules/datas.py
@@ -10,6 +10,13 @@ class transactions:
         self.transactions = dict
         self.number_of_transaction = len(list)
 
+    #returns record of transaction with the transactionID
+    def tranaction(self, transactionID):
+        return self.transactions[transactionID]
+
+    def total_revenue(self):
+        return sum([int(x[11]) for x in self.transactions.values()])
+
 class store_locations:
     def __init__(self, dict):
         self.store_locations = dict
