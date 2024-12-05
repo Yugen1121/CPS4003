b6027f380333090068b15cdf68f156752cb7302c
Yugen1121 <163615963+Yugen1121@users.noreply.github.com>
2024-12-05 10:15:46 +0000

    Remodelled the objects deleated the previous objects and added transactions, store, store_location, payment, payment_method, categories, and category classes

--- assessment2/modules/objects.py
+++ assessment2/modules/objects.py
@@ -1,4 +1,45 @@
+class payment:
+    def __init__(self, method):
+        # takes the input method and assigns it to method
+        self.method = method
+        self.revenue = 0
+
+class payment_methods:
+    def __init__(self):
+        # takes method name as key and payment class as values
+        self.methods = {}
+
+class store:
+    def __init__(self, location):
+        self.store_name = location
+        self.transactionsID = []
+        self.revenue = 0
+        self.rating = 0
+        self.categories = []
+        self.payment_methods = []
+
+class store_locations:
+    def __init__(self):
+        # takes store location as key and stores the store object as values
+        self.store_locations = {}
+
+class category:
+    def __init__(self, name):
+        self.name = name
+        self.category = []
+
+class categories:
+    def __init__(self):
+        # takes category name as key and stores category object as values
+        self.categories = {}
 
 
-
-
+class transactions:
+    def __init__(self):
+        # takes transaction id as key and record as value
+        self.transactions = {}
+        self.revenue = 0
+        self.rating = 0
+        self.stores = store_locations()
+        self.categories = categories()
+        self.payments = payment_methods()
