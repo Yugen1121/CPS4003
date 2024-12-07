d309404f4f4a5fb33ae7ab2b8b725504295a5374
Yugen1121 <163615963+Yugen1121@users.noreply.github.com>
2024-12-07 15:26:32 +0000

    Deleted the sales report function as it is irrelevent now

--- assessment2/modules/datas.py
+++ assessment2/modules/datas.py
@@ -65,30 +65,6 @@ def datas():
 
     return transactions, header
 
-# returns the sales report
-def sales_report(transactions):
-    total_products_sold = 0
-    total_transactions = len(transactions)
-    total_revenue = 0
-    total_customer_satisfaction = 0
-    payment_methods = {}
-    for i in transactions.values():
-        total_products_sold += float(i[5])
-        total_revenue += float(i[11])
-        total_customer_satisfaction += float(i[10])
-        try:
-            payment_methods[i[8]] += 1
-        except KeyError:
-            payment_methods[i[8]] = 1
-
-    for i in payment_methods.keys():
-        payment_methods[i] = (float(payment_methods[i]) / float(total_transactions))*100
-
-    total_customer_satisfaction = total_customer_satisfaction / total_transactions
-    average_transaction_value = total_revenue / total_transactions
-
-    return total_transactions, total_revenue, average_transaction_value,total_products_sold, total_customer_satisfaction, payment_methods
-
 # returns total_revenue_per_location in a dic files where keys are the store location while the value are the total_revenue
 def total_revenue_per_location(transactions, store_location):
     dic = {}
