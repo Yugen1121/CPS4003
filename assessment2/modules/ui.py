a76968d04b06031002d391e1c4d7fd0290f3a61f
Yugen1121 <163615963+Yugen1121@users.noreply.github.com>
2024-12-09 16:40:49 +0000

    Shifterd the dashboard class from objects to here and playing with it

--- assessment2/modules/ui.py
+++ assessment2/modules/ui.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import tkinter as tk
 import modules.datas as datas
+import modules.objects as obj
+
 
 # Function to print a column
 def printcolumn(list, var1, length):
@@ -40,7 +42,40 @@ def print_header(list, length):
         print("|", "="*(length-1), end="")
     print("|")
 
+class Gui:
+    def __init__(self, main, header):
+        self.main = main
+        self.header = header
+        self.window = tk.Tk()
+        self.window.title("Dashboard")
+        self.window.geometry("500x500")
+        self.window.resizable(False, False)
+        self.window.configure(bg="black")
 
+    def column_configure(self, x):
+        for i in range(x):
+            self.window.columnconfigure(i, weight=1)
 
+    def label_configure(self):
+        self.label = tk.Label(self.window)
+        self.label.config(text="Dahsboard", font=("Helvetica", 20))
+        self.label.grid(column=1, row=0, columnspan=2, sticky="ew")
 
+    def click_button1(self, event=None):
+        self.main.pc_revenue_each_location()
 
+    def click_button2(self, event=None):
+        self.main.hg_total_trans_value_el()
+    def button_configure(self):
+        self.button1 = tk.Button(self.window, text="Revenue percentage all location")
+        self.button1.grid(column=1, row=1, columnspan=2, sticky="ew", padx=(5, 5), pady=(5, 5))
+        self.button1.bind("<ButtonRelease-1>", self.click_button1)
+        self.button2 = tk.Button(self.window, text="Histogram of units sold per location")
+        self.button2.grid(column=1, row=2, columnspan=2, sticky="ew", padx=(5, 5), pady=(5, 5))
+        self.button2.bind("<ButtonRelease-1>", self.click_button2)
+
+    def run(self):
+        self.column_configure(4)
+        self.label_configure()
+        self.button_configure()
+        self.window.mainloop()
